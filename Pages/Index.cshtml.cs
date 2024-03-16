aa63e7dd20ab54395560cfaa9971f4cb3122b07c
shulerta <shulerta@email.sc.edu>
2024-03-16 01:05:32 -0400

    first commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContosoPizza.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public TimeSpan TimeInBusiness {get; set; }
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            ViewData["Title"] =  "The Home for Pizza Lovers";
+            TimeInBusiness = DateTime.Now - new DateTime(2018, 8, 14);
+        }
+
+       
+    }
+}
